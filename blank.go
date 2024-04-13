a64d895d29bb23fee4701551c8982b6936aea0eb
krsm274 <toi8x6suke@gmail.com>
2024-04-13 13:26:03 +0900

    first commit(Hello World)

---
+++ blank.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct{}
+
+// NewGame creates a new Game Object and initializes the data.
+func NewGame() *Game {
+	g := &Game{}
+	return g
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(w, h int) (int, int) {
+	return 320, 240
+}
+
+func main() {
+	//g := NewGame()
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
