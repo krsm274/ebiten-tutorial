d252bf0787c34f33c2a488bdfb6893a8d0a4ebe5
krsm274 <toi8x6suke@gmail.com>
2024-04-16 19:38:54 +0900

    座標を指定して画像をレンダリングする

---
+++ set_image_position/set_position.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+var img *ebiten.Image
+
+func init() {
+	var err error
+	// give the "relative" path to the image.
+	img, _, err = ebitenutil.NewImageFromFile("ebiten.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Game struct{}
+
+// NewGame creates a new Game Object and initializes the data.
+func NewGame() *Game {
+	g := &Game{}
+	return g
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(150, 50)
+	op.GeoM.Scale(1.5, 1)
+	screen.DrawImage(img, op)
+}
+
+func (g *Game) Layout(w, h int) (int, int) {
+	return 320, 240
+}
+
+func main() {
+	g := NewGame()
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Set Position")
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
+}
